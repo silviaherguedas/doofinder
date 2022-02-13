e04f181601b73b9f0ee771413d65ef84197a8d96
sherguedas <sherguedas@ext.elconfidencial.com>
2022-02-13 14:41:21 +0100

    Refactor data dump from csv against your table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,11 +10,5 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-# Seed `authors` table from CSV
-Mylibrary.CSVHelper.column_data("priv/repo/data/authors.csv")
-
-# Seed `publishers` table from CSV
-Mylibrary.CSVHelper.column_data("priv/repo/data/publishers.csv")
-
-# Seed `languages` table from CSV
-Mylibrary.CSVHelper.column_data("priv/repo/data/languages.csv")
+# Data dump from CSV against your table
+for filename <- ["authors","publishers", "languages"], do: Mylibrary.CSVHelper.column_data("priv/repo/data/" <> filename <> ".csv")
